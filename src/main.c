7400b395409d63cadc5373a356f38c1d0cab0448
stercusmax <stercusmax@yahoo.com>
2025-08-02 22:35:42 +0200

    Working on macros arguments

--- src/main.c
+++ src/main.c
@@ -45,6 +45,7 @@ int hashstring(const char* s) {
 struct macro {	
 	char name[MAXNAME];
 	char * bfi;
+	unsigned * argsigns;
 	unsigned narg;
 };
 
@@ -62,13 +63,15 @@ void freesubpointers(struct listmacro * p)
 			continue;
 		if (p[i].size) printf("-----------------\n");
 		for (j = 0; j < p[i].size; j++) {
+			putchar(p[i].macrolist[j].bfi[1]);
 			printf("listmacro[%d] and listmacro.macrolist[%d] name: %s\nbfi = %s\n", i, j, p[i].macrolist[j].name, 
 			(p[i].macrolist[j].bfi) ? p[i].macrolist[j].bfi : "NULL");
 			if (p[i].macrolist[j].bfi)
 				free(p[i].macrolist[j].bfi);
+			if (p[i].macrolist[j].argsigns)
+				free(p[i].macrolist[j].argsigns);
 		}
 		free(p[i].macrolist);
-		if (p[i].size) printf("-----------------\n");
 	}
 }
 
@@ -82,15 +85,6 @@ int findelement(struct listmacro list, char * name)
 	return 0;
 }
 
-#define KEYWORDCHECK(i, equalsign, comparison) (s[i] equalsign 'D' comparison s[i] \
-equalsign 'U' comparison s[i] equalsign 'I' comparison s[i] equalsign '$' comparison s[i] equalsign '"' comparison s[i] equalsign '%')
-
-#define ERROR(message, givenbackuppos, ret, additionalformat) {freesubpointers(macros);\
-fprintf(stderr, message, additionalformat name, givenbackuppos.line + 1, givenbackuppos.collumn + 1);  return ret;} 
-
-#define COMMA ,
-
-
 struct listmacro macros[MAXMACROS] = { };
 
 struct pos {
@@ -100,6 +94,7 @@ struct pos {
 
 #define INITIALLIST 4
 #define INITIALMBFI 100
+#define INITIALARGSIGN 10
 
 void * allocate(size_t size)
 {
@@ -118,11 +113,13 @@ void * reallocate(void * pr, size_t size)
 void initializemacrolist(struct macro * macrolist, int first, int last) //first and last included
 {
 	int i;
-	for (i = first; i <= last; i++)
+	for (i = first; i <= last; i++) {
 		macrolist[i].bfi = NULL; 
+		macrolist[i].argsigns = NULL;		
+	}
 }
 
-int processmacros(char * code, char * output, char *name, int *outputpos, unsigned *i, struct pos *currentpos);
+int processmacros(char * code, char * output, char *name, unsigned *outputpos, unsigned *i, struct pos *currentpos);
 
 int preprocess(char * s, char * output, char * name)
 {
@@ -131,8 +128,7 @@ int preprocess(char * s, char * output, char * name)
 			macros[i].macrolist = NULL; //will have to malloc to create a list, in this list bfi should be null if there's no instruction, or malloc
 			macros[i].size = 0;
 		}
-	unsigned i;
-	int outputpos ,outputwrite = 0;
+	unsigned i,  outputpos ,outputwrite = 0;
 	struct pos currentpos = {.line = 0, .collumn = 0};
 	for (outputpos = i = 0; s[i]; i++, currentpos.collumn++)
 		switch(s[i])
@@ -156,15 +152,23 @@ int preprocess(char * s, char * output, char * name)
 	return 0;	
 }
 
-int processmacros(char * s, char * output, char *name, int *outputpos, unsigned *i, struct pos *currentpos)
+#define KEYWORDCHECK(i, equalsign, comparison) (s[i] equalsign 'D' comparison s[i] \
+equalsign 'U' comparison s[i] equalsign 'I' comparison s[i] equalsign '$' comparison s[i] equalsign '"' comparison s[i] equalsign '%')
+
+#define ERROR(message, givenbackuppos, ret, additionalformat) {freesubpointers(macros);\
+fprintf(stderr, message, additionalformat name, givenbackuppos.line + 1, givenbackuppos.collumn + 1);  return ret;} 
+
+#define COMMA ,
+
+int processmacros(char * s, char * output, char *name, unsigned *outputpos, unsigned *i, struct pos *currentpos)
 {
 	switch (s[(*i) + 1])
 	{
 	case 'D': case 'U': case 'I': case '$': case '"':  case '%':
 		struct pos backuppos = (*currentpos);
 		++(*i); currentpos->collumn++;
-		unsigned oldi = (*i), j = 0 ,colon = 0;
-		unsigned char endc;
+		unsigned oldi = (*i), j = 0, colon = 0;
+		char endc;
 		char * argp = NULL, macroname[MAXNAME];
 		int parenthese = 0, bfi = -1;
 		if (s[(*i)] == '"') {
@@ -179,20 +183,31 @@ int processmacros(char * s, char * output, char *name, int *outputpos, unsigned 
 			if (s[(*i)] == endc) {
 				if (endc == '"' && s[(*i) - 1] == '\\') 
 					continue;
-				else if (s[oldi] == 'D' || s[oldi] == 'I') {
+				else if ((s[oldi] == 'D' || s[oldi] == 'I') && colon) {
 					struct pos backuppos2 = (*currentpos);
 					(*i)++;
 					if (KEYWORDCHECK((*i), !=, &&)) {(*i)--; break;}
 					(*i)++;
+					int quotedstring = 0;
 					if (s[(*i) - 1] == 'D' || s[(*i) - 1] == 'I') { //with : after
-						int stack;
-						for (stack = 1; s[(*i)] && stack; (*i)++, currentpos->collumn++) {
+						char lastthing;
+						int stack, colon2;
+						for (lastthing = s[(*i) - 1], stack = 1, colon2 = 0; s[(*i)] && stack; (*i)++, currentpos->collumn++) {
 							if (s[(*i)] == '#') {
-								if (KEYWORDCHECK((*i) + 1, ==, ||))
+								if (KEYWORDCHECK((*i) + 1, ==, ||)) {
+									if ((lastthing == 'D' || lastthing == 'I') && !colon2) {stack--; continue;}
+									else if (lastthing == 'D' || lastthing == 'I') colon2 = 0;
+									if (s[*i + 1] == '"')
+										quotedstring = 1;
+									else
+										quotedstring = 0;
 									stack++;
+									lastthing = s[(*i) + 1];
+								}
 								else
 									stack--;
 							}
+							else if (!colon2 && s[(*i)] == ':') colon2 = 1;
 							else if (s[(*i)] == '\n' ) {currentpos->line++; currentpos->collumn = 0;}
 						}
 						if (!stack) {(*i)--; currentpos->collumn--;}
@@ -202,13 +217,15 @@ int processmacros(char * s, char * output, char *name, int *outputpos, unsigned 
 							if (s[(*i)] == '#') break;
 							else if (s[(*i)] == '\n' ) {currentpos->line++; currentpos->collumn = 0;}
 						}
-					else if (s[(*i) - 1] == '"') //different stuff
-						for (; s[(*i)] && s[(*i)] != '"'; (*i)++, currentpos->collumn++) {
+					else if (s[(*i) - 1] == '"') { //different stuff
+						quotedstring = 1;
+						for (; s[(*i)]; (*i)++, currentpos->collumn++) {
 							if (s[(*i)] == '"' && s[(*i) - 1] != '\\')
 								break;
 							else if (s[(*i)] == '\n' ) {currentpos->line++; currentpos->collumn = 0;}
 						}
-					if (!s[(*i)]) ERROR("Not closed # in %s line : %ld, collumn : %ld\n", backuppos2, -1,)
+					}
+					if (!s[(*i)]) ERROR("Not closed %c in %s line : %ld, collumn : %ld\n", backuppos2, -1, (quotedstring) ? '"' : '#' COMMA)
 				}
 				else
 					break;
@@ -218,32 +235,28 @@ int processmacros(char * s, char * output, char *name, int *outputpos, unsigned 
 				colon = 1; 
 				bfi = *i + 1;
 			}
-			else if (s[(*i)] == ')' && parenthese) {
-				parenthese = 0;
-			}
-			else
-				 currentpos->collumn++;
-			if (s[oldi] == 'D' || s[oldi] == 'I') {
-				if (colon)
+			if ((s[oldi] == 'D' || s[oldi] == 'I') && colon)
 					continue;
-			}
-			if (s[oldi] == 'D' || s[oldi] == '$') {
+			if (s[(*i)] == ')' && parenthese)
+				parenthese = 0;
+			else if (s[oldi] == 'D' || s[oldi] == '$') {
 				if (s[(*i)] == '(' && !parenthese) {
-					if (!s[(*i) + 1] || s[(*i)+1] == '#') ERROR("Not closed parentheses in %s line : %ld, collumn : %ld\n", backuppos, -1, ) 
 					argp = &s[(*i) + 1]; parenthese = 1;
 				}
 			}
-			if(s[oldi] != '"') {
+			if(s[oldi] != '"')
 				if (!isspace(s[(*i)]))
-					macroname[j++] = s[(*i)];	
-			}
+					macroname[j++] = s[(*i)];
 		}
 		if (!s[(*i)]) ERROR("Not closed %c in %s line : %ld, collumn : %ld\n", backuppos, -1, endc COMMA) 
 		else if (parenthese) ERROR("Not closed parentheses in %s line : %ld, collumn : %ld\n", backuppos, -1, ) 
-		macroname[j] = '\0'; 
+		macroname[j] = '\0'; //j will now serve as a loop i
 		unsigned hashedstring = hashstring(macroname), 
 		foundelement = findelement(macros[hashedstring], macroname),
 		pos = (foundelement) ? foundelement - 1 : macros[hashedstring].size;
+		char passedoutput[CODESIZE];
+		int retval;
+		unsigned k, passedoutputpos = 0;
 		if (s[oldi] == 'D') {
 			if (!foundelement) {
 				if (!macros[hashedstring].macrolist) {
@@ -262,15 +275,36 @@ int processmacros(char * s, char * output, char *name, int *outputpos, unsigned 
 			if (bfi != -1) {
 				if (!macros[hashedstring].macrolist[pos].bfi)
 					macros[hashedstring].macrolist[pos].bfi = allocate(INITIALMBFI);
-				if (*i - bfi >= INITIALMBFI)
-					macros[hashedstring].macrolist[pos].bfi = reallocate(macros[hashedstring].macrolist[pos].bfi, INITIALMBFI * ((*i - bfi) / INITIALMBFI + 1));
-				strncpy(macros[hashedstring].macrolist[pos].bfi, s + bfi, *i - bfi);
-				macros[hashedstring].macrolist[pos].bfi[*i - bfi] = '\0';		
-			}
-			else {
-				if (macros[hashedstring].macrolist[pos].bfi) {
-					free(macros[hashedstring].macrolist[pos].bfi); macros[hashedstring].macrolist[pos].bfi = NULL;
+
+				 //replace strings macros , $ and % macros in bfi
+				unsigned l, encounteredchar = 0;
+				for (j = bfi, l = 0; j < *i; j++, l++) {
+					if (!encounteredchar && isspace(s[j])) {l--; continue;}
+					if (s[j] == '\n') encounteredchar = 0;
+					else encounteredchar = 1;
+					if (s[j] == '#' && (s[j + 1] == '"' || s[j + 1] == '$' || s[j + 1] == '%')) {
+						retval =  processmacros(s, passedoutput, name, &passedoutputpos, &j, currentpos);
+						if (retval) return retval;
+						for (k = 0; k < passedoutputpos; k++, l++) {
+							if ((l + 1) % INITIALMBFI) //for null char
+								macros[hashedstring].macrolist[pos].bfi = reallocate(macros[hashedstring].macrolist[pos].bfi, 
+								INITIALMBFI * ((l / INITIALMBFI) + 1));
+							macros[hashedstring].macrolist[pos].bfi[l] = passedoutput[k];
+						}
+						if (k) l--;
+						passedoutputpos = 0;
+					}
+					else {
+						if ((l + 1) % INITIALMBFI) //for null char
+							macros[hashedstring].macrolist[pos].bfi = reallocate(macros[hashedstring].macrolist[pos].bfi, 
+							INITIALMBFI * ((l / INITIALMBFI) + 1));
+						macros[hashedstring].macrolist[pos].bfi[l] = s[j];
+					}		
 				}
+				macros[hashedstring].macrolist[pos].bfi[l] = '\0';
+			}
+			else if (macros[hashedstring].macrolist[pos].bfi) {//if defined without ":" after
+				free(macros[hashedstring].macrolist[pos].bfi); macros[hashedstring].macrolist[pos].bfi = NULL;
 			}
 			macros[hashedstring].macrolist[pos].narg = 0;
 			if (argp) {
@@ -291,21 +325,47 @@ int processmacros(char * s, char * output, char *name, int *outputpos, unsigned 
 				free(macros[hashedstring].macrolist[pos - 1].bfi);
 			macros[hashedstring].size--;
 		}
-		else if (s[oldi] == '$') {
+		else if (s[oldi] == '$') { 
 			if (!foundelement) ERROR("Using an undefined macro \"%s\" in %s in line : %ld, collumn : %ld\n", backuppos, -1, macroname COMMA)
-			/*if (!macros[hashedstring].macrolist[pos].bfi)
+			if (!macros[hashedstring].macrolist[pos].bfi) 
 				break;
 			if (argp) {
-				unsigned args;
-				for (; *argp != ')'; argp++)
-					if (*argp == ',')
-						args++;
-				args++;
+				if (!macros[hashedstring].macrolist[pos].narg)
+					ERROR("Adding arguments to a macro: \"%s\" who doesn't have argument in %s in line : %ld, collumn : %ld\n", backuppos, -1, macroname COMMA)
+				unsigned currentarg, negative;
+				int cellpos;
+				for (cellpos = 0, currentarg = 1; *argp != ')'; argp++) {
+					if (*argp == ',') {
+						if (cellpos < 0) {cellpos = -cellpos; negative = 1;}
+						else negative = 0;
+						for (k = 0; k < (unsigned) cellpos; k++)
+							if (negative)
+								output[(*outputpos)++] = '<';
+							else
+								output[(*outputpos)++] = '>';
+						currentarg++;
+					}
+					else if (*argp == '<')
+						cellpos--;
+					else if (*argp == '>')
+						cellpos++;	
+				}
 			}
-			bfi = macros[hashedstring].macrolist[pos].bfi;
-			for (; *bfi; bfi++)
-				if (!isspace(*bfi))
-					output[(*outputpos)++] = *bfi;*/
+			else if (macros[hashedstring].macrolist[pos].narg)
+				ERROR("Putting no arguments to a macro: \"%s\" who has argument(s) in %s in line : %ld, collumn : %ld\n", backuppos, -1, macroname COMMA)
+			//get the content of the macro and put it in the file, macro execute the U,I,IN
+			for (bfi = 0; macros[hashedstring].macrolist[pos].bfi[bfi]; bfi++) {
+				if (s[bfi] == '#' && (s[bfi + 1] == 'U' || s[bfi + 1] == 'I')) {
+					retval = processmacros(s, passedoutput, name, &passedoutputpos, &j, currentpos);
+					if (retval) return retval;
+					for (k = 0; k < passedoutputpos; k++, (*outputpos)++)
+						output[*outputpos] = passedoutput[k];
+					if (k) (*outputpos)--;
+					passedoutputpos = 0;
+				}
+				else
+					output[(*outputpos)++] = macros[hashedstring].macrolist[pos].bfi[bfi];		
+			}
 		}
 		else if (s[oldi] == '%') {
 			char file[CODESIZE], macroreplaced[OUTPUTSIZE];
@@ -313,14 +373,14 @@ int processmacros(char * s, char * output, char *name, int *outputpos, unsigned 
 		}
 		else if (s[oldi] == '"') {
 			char temp;
-			/*for (; s[bfi]!='"' && s[bfi - 1] == '\\'; bfi++) {
-				for (temp = bfi; temp; temp--)
+			for (; s[bfi]!='"' || s[bfi - 1] == '\\'; bfi++) {
+				for (temp = s[bfi]; temp; temp--)
 					output[(*outputpos)++] = '+';
 				output[(*outputpos)++] = ' ';
-				output[(*outputpos)++] = *bfi;
+				output[(*outputpos)++] = s[bfi];
 				output[(*outputpos)++] = '\n';	
 				output[(*outputpos)++] = '>';
-			}*/
+			}
 		}
 		break;
 	default:
